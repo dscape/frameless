86f78e95118706d0e4c945c4c014e33cf47ff039
Nuno Job <nunojobpinto@gmail.com>
2013-01-17 17:39:23 -0800

    things

--- test/encrypt-test.js
+++ test/encrypt-test.js
@@ -1,55 +1,64 @@
 var clieasy   = require('cli-easy')
   , assert    = require('assert')
   , path      = require('path')
-  , encrypt   = path.join(__dirname, '../usage/encrypt-aes')
+  , fs        = require('fs')
+  , crypto    = require('crypto')
+  , bin       = path.join(__dirname, '../usage/encrypt-aes')
   , frameless = require('../lib/frameless')
+  , key       = fs.readFileSync(
+    path.join(__dirname, 'keys', 'id_rsa'), 'utf-8')
   ;
 
-//clieasy.describe('usage/encrypt')
-//  .use(encrypt)
-//  .discuss('when using `usage/encrypt-aes`')
-//  .discuss('with all required options')
-//    .arg('-k ' + path.join(frameless.tilde, '.ssh', 'id_rsa'))
-//    .expect('should be able to parse the user', function (h) {
-//      var lines = h.split('\n');
-//      dotfile_path = lines.shift();
-//      response = JSON.parse(lines.join('\n'));
-//      return true;
-//    })
-//    .expect('response user id should be `maciej`', function () {
-//      return response.id && ~response.id.indexOf('maciej');
-//    })
-//    .expect('response should have been ok', function () {
-//      return response.ok;
-//    })
-//    .expect('dot file should exist', function () {
-//      dotfile = fs.readFileSync(dotfile_path, 'utf-8');
-//      return true;
-//    })
-//    .expect('dot file should be valid json', function () {
-//      dotfile = JSON.parse(dotfile);
-//      return true;
-//    })
-//    .expect('protocol to be `http`', function () {
-//      return dotfile.protocol === 'http';
-//    })
-//    .expect('`usr-name` not to be saved', function () {
-//      return !dotfile['usr-name'];
-//    })
-//    .expect('`usr-pwd` not to be saved', function () {
-//      return !dotfile['usr-pwd'];
-//    })
-//    .expect('`usr-email` not to be saved', function () {
-//      return !dotfile['usr-email'];
-//    })
-//    .expect('hostname to be encrypted', function () {
-//      encrypted.hostname = dotfile.hostname;
-//      return dotfile.hostname !== 'localhost:5984';
-//    })
-//    .expect('password to be encrypted', function () {
-//      encrypted.password = dotfile.password;
-//      return dotfile.password !== 'food';
-//    })
-//    .expect('foobar', function () { return true; })
-//["export"](module);
-//
+function encrypt(text) {
+  var cipher = crypto.createCipher('aes-256-cbc', key);
+  var crypted = cipher.update(text,'utf8', 'hex');
+  crypted += cipher.final('hex');
+  return crypted;
+}
+
+function decrypt(text) {
+  var decipher = crypto.createDecipher('aes-256-cbc', key);
+  var dec = decipher.update(text,'hex', 'utf8');
+  dec += decipher.final('utf8');
+  return dec;
+}
+
+clieasy.describe('usage/encrypt')
+  .use(
+    [ 'echo "a" | '
+    , bin + ' -k ' + path.join(__dirname, 'keys', 'id_rsa')
+    ].join(''))
+  .discuss('when using `usage/encrypt-aes`')
+  .discuss('with our test key')
+    .expect('should encrypt the same as node does', function (h) {
+      return h === encrypt("a\n");
+    })
+["export"](module);
+
+clieasy.describe('usage/decrypt')
+  .use(
+    [ 'echo "' + encrypt('a\n') + '" | '
+    , bin + ' -d -k ' + path.join(__dirname, 'keys', 'id_rsa')
+    ].join(''))
+  .discuss('when using `usage/encrypt-aes`')
+  .discuss('with our test key')
+    .expect('should decrypt the same as node does', function (h) {
+      return h === "a\n";
+    })
+["export"](module);
+
+var cycle = 
+  [ 'echo "' + encrypt('a\n') + '" | '
+  , bin + ' -d -k ' + path.join(__dirname, 'keys', 'id_rsa') + ' | '
+  , bin + ' -k ' + path.join(__dirname, 'keys', 'id_rsa') + ' | '
+  , bin + ' -d -k ' + path.join(__dirname, 'keys', 'id_rsa')
+  ].join('');
+
+clieasy.describe('usage/cycle')
+  .use(cycle)
+  .discuss('when using `usage/encrypt-aes`')
+  .discuss('with our test key')
+    .expect('should cycle isomorphically', function (h) {
+      return h === "a\n";
+    })
+["export"](module);
